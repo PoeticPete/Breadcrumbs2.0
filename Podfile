b6a61d8f9e577de6ee4140d2f05534ffdb27b142
Peter Tao <petertao@peters-macbook-pro.local>
2016-10-18 11:31:25 -0400

    Minor changes to try to get subview to respond to touch

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target “Breadcrumbs 2.0” do
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+end
---
+++ Pods/Target Support Files/Pods-Breadcrumbs 2.0/Pods-Breadcrumbs 2.0-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Breadcrumbs_2_0VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Breadcrumbs_2_0VersionString[];
+
