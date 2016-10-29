e76786741c7b5a18f58b7b0bbaed47a3409587d5
Peter Tao <petertao@1x-nat-vl930-172-30-66-117.wireless.umass.edu>
2016-10-28 21:25:19 -0400

    deleted some useless files

---
+++ Breadcrumbs 2.0/PhotoCalloutView.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCalloutView.swift
+//  Breadcrumbs 2.0
+//
+//  Created by Peter Tao on 10/28/16.
+//  Copyright © 2016 Poetic Pete. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCalloutView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
